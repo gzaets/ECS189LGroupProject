56ce2a594a67d02f31412c99b0214ed9f1e077bd
ComradeConejo <georgezaets@gmail.com>
2023-05-19 20:12:37 -0700

    Adding new asset to use as place holder for the player during dev,
    Also, moving all scenes to Scenes folder

--- Sovereign Saga/Assets/Scripts/PlayerController.cs
+++ Sovereign Saga/Assets/Scripts/PlayerController.cs
@@ -4,16 +4,21 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float moveSpeed = 5f;
-    public Rigidbody2D rb;
+    public float speed = 5f;
+    private Rigidbody2D rb;
     public Animator animator;
 
     Vector2 movement;
 
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
     void Update()
     {
-        movement.x = Input.GetAxisRaw("Horizontal");
-        movement.y = Input.GetAxisRaw("Vertical");
+        movement.x = Input.GetAxisRaw("Horizontal") * 0.75f;
+        movement.y = Input.GetAxisRaw("Vertical") * 0.75f;
 
         if (movement != Vector2.zero)
         {
@@ -26,6 +31,15 @@ public class PlayerController : MonoBehaviour
 
     void FixedUpdate()
     {
-        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector2 movement = new Vector2(moveHorizontal, moveVertical);
+        rb.velocity = movement * speed;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        
     }
 }
