c4f2a70e0d97e6b12f09568fdb2c91c54b380e3a
fdang2000 <fdang@ucdavis.edu>
2023-06-04 16:41:21 -0700

    Added dashing.

--- Sovereign Saga/Assets/Scripts/PlayerController.cs
+++ Sovereign Saga/Assets/Scripts/PlayerController.cs
@@ -4,22 +4,27 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float speed = 5f;
+    private float currentMovementSpeed = 4.0f;
+    private float defaultMovementSpeed = 4.0f;
+    
+    // Dashing Variables
+    private float dashSpeed = 8.0f;
+    private float dashLength = 0.5f;
+    private float dashCooldown = 10.0f;
+    private float dashCounter = 0.0f;
+    private bool canDash = true;
+
     private Rigidbody2D rb;
-    public Animator animator;
-
-    Vector2 movement;
-
+    private Animator animator;
+    private Vector2 movement;
     private Vector2 mouseLocation;
-    private int lastMovement;
-
     private WeaponController weaponController;
 
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
         weaponController = GetComponentInChildren<WeaponController>();
-
+        animator = GetComponentInChildren<Animator>();
 
         // Set it so the player faces down at the beginning of the game. 
         animator.SetFloat("Horizontal", 0);
@@ -42,13 +47,35 @@ public class PlayerController : MonoBehaviour
             animator.SetFloat("Speed", rb.velocity.magnitude);
         }
 
-
         mouseLocation = GetMousePosition();
         weaponController.setPointerPosition(mouseLocation);
         
+        // Attacking
         if (Input.GetButton("Fire1"))
         {
             weaponController.Attack();
+        }
+
+        // Dashing
+        if (Input.GetKeyDown(KeyCode.Space) && canDash)
+        {
+            canDash = false;
+            currentMovementSpeed = dashSpeed;
+        }
+
+        if (!canDash)
+        {   
+            dashCounter += Time.deltaTime;
+            if (dashCounter >= dashLength)
+            {
+                currentMovementSpeed = defaultMovementSpeed;
+            }
+
+            if (dashCounter >= dashCooldown)
+            {
+                canDash = true;
+                dashCounter = 0.0f;
+            }
         }
 
     }
@@ -58,8 +85,8 @@ public class PlayerController : MonoBehaviour
         float moveHorizontal = Input.GetAxis("Horizontal");
         float moveVertical = Input.GetAxis("Vertical");
 
-        Vector2 movement = new Vector2(moveHorizontal, moveVertical).normalized;
-        rb.velocity = movement * speed;
+        Vector2 direction = new Vector2(moveHorizontal, moveVertical).normalized;
+        rb.velocity = direction * currentMovementSpeed;
 
     }
 
