bc8cf01860c84743069ad65e0aafff75888c4cbc
ComradeConejo <georgezaets@gmail.com>
2023-06-10 17:54:53 -0700

    slime face direction fix

--- Sovereign Saga/Assets/Scripts/Slime.cs
+++ Sovereign Saga/Assets/Scripts/Slime.cs
@@ -8,17 +8,34 @@ public class Slime : MonoBehaviour
     public int damage = 10;
     public Transform target;
     private Rigidbody2D rb;
+    private SpriteRenderer spriteRenderer;  // Used to flip the sprite
 
     void Start()
     {
         // Assuming the player has a tag of "Player"
-        //target = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+        // target = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+
+        // Get the sprite renderer component
+        spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     void Update()
     {
         // Move towards the player
-        transform.position = Vector2.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
+        Vector2 targetPosition = target.position;
+        transform.position = Vector2.MoveTowards(transform.position, targetPosition, speed * Time.deltaTime);
+        
+        // Flip the sprite based on the direction of movement
+        if (targetPosition.x > transform.position.x)
+        {
+            // Target is to the right of the slime, so slime should face right
+            spriteRenderer.flipX = false;
+        }
+        else if (targetPosition.x < transform.position.x)
+        {
+            // Target is to the left of the slime, so slime should face left
+            spriteRenderer.flipX = true;
+        }
     }
 
     void OnCollisionEnter2D(Collision2D collision)
