8c7862640b22433ff5026390a91cffbb454677cd
ComradeConejo <georgezaets@gmail.com>
2023-06-14 01:34:14 -0700

    pause menu working

--- Sovereign Saga/Assets/Scripts/PauseMenu.cs
+++ Sovereign Saga/Assets/Scripts/PauseMenu.cs
@@ -4,15 +4,37 @@ using UnityEngine;
 
 public class PauseMenu : MonoBehaviour
 {
+    public GameObject pauseMenu;
     // Start is called before the first frame update
+
+
     void Start()
     {
-        
+        pauseMenu.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Escape) && !pauseMenu.activeSelf)
+        {
+            PauseGame();
+        }
+        else if (Input.GetKeyDown(KeyCode.Escape) && pauseMenu.activeSelf)
+        {
+            ResumeGame();
+        }
+    }
+
+    public void PauseGame()
+    {
+        pauseMenu.SetActive(true);
+        Time.timeScale = 0f;
+    }
+
+    public void ResumeGame()
+    {
+        pauseMenu.SetActive(false);
+        Time.timeScale = 1f;
     }
 }
